df9b7b7172fc4f2e951f1b5df12efc96228f3c78
Abdelrahman Ashraf <bodyboda236@gmail.com>
2024-05-14 19:06:25 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+// import 'bindings/initial_binding.dart';
+import 'view/signup.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      // initialBinding: InitialBingings(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: SignUpPage(),
+    );
+  }
+}
